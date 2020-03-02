ddc35e7f6c0a37718947b00c6f7bf447d6b3c724
csedome <csedome@gmail.com>
2020-03-02 16:07:10 +0100

    Fixed error and added JUnit test

---
+++ train-controller/src/test/java/hu/bme/mit/train/controller/MainTest.java
@@ -0,0 +1,20 @@
+package hu.bme.mit.train.controller;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class MainTest {
+
+    @Before
+    public void before() {
+        // TODO Add initializations
+    }
+
+    @Test
+    public void ThisIsAnExampleTestStub() {
+        // TODO Delete this and add test cases based on the issues
+	Assert.assertEquals(0, 0);
+    }
+}
