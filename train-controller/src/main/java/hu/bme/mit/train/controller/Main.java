ac856b04245a893c11f11a7f3b27c3ac94eca04d
csedome <csedome@gmail.com>
2020-03-02 15:38:29 +0100

    Hello printing changed to branch-B

--- train-controller/src/main/java/hu/bme/mit/train/controller/Main.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/Main.java
@@ -2,6 +2,6 @@ package hu.bme.mit.train.controller;
 
 public class Main{
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello branch-B!");
 	}
 }
