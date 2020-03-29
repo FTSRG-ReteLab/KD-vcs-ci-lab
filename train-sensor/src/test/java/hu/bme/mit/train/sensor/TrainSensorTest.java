30504b63172d5360d6b98cec2228305539f7ece7
jkinga <jenei.kinga.s@gmail.com>
2020-03-29 17:55:44 +0200

    Added tests

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,5 +1,7 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,13 +9,39 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainController mockTrainController;
+    TrainUser mockTrainUser;
+    TrainSensorImpl trainSensor;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        mockTrainController = mock(TrainController.class);
+        mockTrainUser = mock(TrainUser.class);
+        trainSensor = new TrainSensorImpl(mockTrainController, mockTrainUser);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void AbsoluteMargin1() {
+        trainSensor.overrideSpeedLimit(-50);
+        verify(mockTrainUser, times(1)).setAlarmState(true);
     }
+
+    @Test
+    public void AbsoluteMargin2() {
+        trainSensor.overrideSpeedLimit(550);
+        verify(mockTrainUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void RelativeMargin1() {
+        trainSensor.overrideSpeedLimit(1);
+        verify(mockTrainUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void RelativeMargin2() {
+        trainSensor.overrideSpeedLimit(4);
+        verify(mockTrainUser, times(0)).setAlarmState(false);
+    }
+
 }
